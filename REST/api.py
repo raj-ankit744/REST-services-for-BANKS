1f6aeb4665fa35e4b67adfdf54dfaac11576a08c
ANKIT RAJ GUPTA <arg@MacBook-Air.local>
2019-07-02 20:16:22 +0530

    GET requesting using query parameter

--- REST/api.py
+++ REST/api.py
@@ -7,9 +7,10 @@ from flask_jwt_extended import jwt_required
 
 banks = Blueprint('banks', __name__, url_prefix='/api')
 
-@banks.route('/getBankDetails/<ifsc>', methods=['GET'])
+@banks.route('/getBankDetails', methods=['GET'])
 @jwt_required
-def get_bank_details(ifsc):
+def get_bank_details():
+    ifsc = request.args.get('ifsc')
     query = "SELECT * FROM bank_branches WHERE ifsc='%s'"%(ifsc)
     try:
         res = select_query(query).fetchone()
@@ -21,9 +22,13 @@ def get_bank_details(ifsc):
         return str(e)
     return jsonify(res)
 
-@banks.route('/getBranchDetails/<bank_name>/<city>/<int:limit>/<int:offset>')
+@banks.route('/getBranchDetails')
 @jwt_required
-def get_branch_details(bank_name, city, limit, offset):
+def get_branch_details():
+    bank_name = request.args['bank_name']
+    city = request.args['city']
+    limit = int(request.args['limit'])
+    offset = int(request.args['offset'])
     query = "SELECT address, branch, district, ifsc, state  FROM bank_branches WHERE bank_name='%s' AND city='%s' LIMIT %d OFFSET %d"%(bank_name, city, limit, offset)
     try:
         res = select_query(query).fetchall()
